9f8ff2f91d1e4e9b7ad49422aa4c4871623ad1e2
karanmistry007 <ksmistry007@gmail.com>
2024-09-24 00:46:56 +0530

    [Chore]: Kanban Update Major Changes Only Dynamic Pending...

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -1,68 +1,50 @@
 import { useState } from 'react'
-import { Task } from '../utils/data-tasks'
-
-const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
-</svg>
-const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
-</svg>
-const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
-</svg>
+import { Task } from '@/utils/data-tasks'
+import { Card } from '@/components/ui/card'
+import { Textarea } from "@/components/ui/textarea"
 
 const TaskCard = ({ task, updateTask }: {
     task: Task
     updateTask: (task: Task) => void
 }) => {
     const [isEditingTitle, setIsEditingTitle] = useState(false)
-    const points = task.points || 0
-    const updatePoints = (direction: 'up' | 'down') => {
-        const fib = [0, 1, 2, 3, 5, 8, 13]
-        const index = fib.indexOf(points)
-        const nextIndex = direction === 'up' ? index + 1 : index - 1
-        const newPoints = fib[nextIndex]
-        if (newPoints) {
-            updateTask({ ...task, points: newPoints })
-        }
-    }
-    return <div
-        draggable
-        onDragStart={(e) => {
-            e.dataTransfer.setData("id", task.id)
-        }}
-        className="border rounded-lg px-2 m-2 bg-gray-50 w-56"
-    >
-        <div className="text-base font-base py-2">
-            {isEditingTitle ? (
-                <input
-                    autoFocus
-                    className="w-full"
-                    onBlur={() => setIsEditingTitle(false)}
-                    value={task.title}
-                    onChange={(e) => updateTask({ ...task, title: e.target.value })}
-                />
-            ) : (
-                <div onClick={() => setIsEditingTitle(true)}>
-                    {task.title}
-                </div>
-            )}
-        </div>
-        <div className="flex gap-4 justify-between py-2 text-gray-500 text-sm">
-            <div className="flex gap-2">
-                <div>{task.id}</div>
-                {task.priority === 'high' && highPriorityIcon}
-                {task.priority === 'medium' && mediumPriorityIcon}
-                {task.priority === 'low' && lowPriorityIcon}
+
+    return (
+        <>
+            <div
+                draggable
+                onDragStart={(e) => {
+                    e.dataTransfer.setData("id", task.id)
+                }}
+                className="rounded-lg m-5 bg-gray-50 w-[calc(100%_-_40px)]"
+            >
+                <Card className="p-2 m-0 min-h-[100px]">
+                    {isEditingTitle ? (
+                        <div className='cursor-pointer'>
+                            <Textarea
+                                autoFocus
+                                className="w-full min-h-[82px]"
+                                onBlur={() => setIsEditingTitle(false)}
+                                value={task.title}
+                                onChange={(e) => updateTask({ ...task, title: e.target.value })}
+                                onKeyDown={(e) => {
+                                    e.key === "Enter" ? setIsEditingTitle(false) : "";
+                                }}
+                            ></Textarea>
+                        </div>
+                    ) : (
+                        <div className='cursor-pointer min-h-[80px]'
+                            onDoubleClick={() => setIsEditingTitle(true)}
+                        >
+                            <p className="leading-7">
+                                {task.title}
+                            </p>
+                        </div>
+                    )}
+                </Card>
             </div>
-            <div className="flex gap-2 items-center">
-                <button onClick={() => updatePoints('down')}>-</button>
-                <div className="font-bold">{points}</div>
-                <button onClick={() => updatePoints('up')}>+</button>
-            </div>
-        </div>
-    </div>
+        </>
+    )
 }
 
 export default TaskCard
--- frontend/src/pages/KanbanView.tsx
+++ frontend/src/pages/KanbanView.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import TaskCard from '../components/TaskCard'
 import { Status, statuses, Task } from '../utils/data-tasks'
 
-function KanbanView() {
+const KanbanView = () => {
     const [tasks, setTasks] = useState<Task[]>([
         {
             title: "This is name of the task 1",
@@ -28,7 +28,7 @@ function KanbanView() {
         {
             title: "This is name of the task 1",
             id: "4",
-            status: "Cancelled",
+            status: "Closed",
             priority: "low",
             points: 1
         },
@@ -80,34 +80,47 @@ function KanbanView() {
         <>
             {/* DASHBOARD CONTAINER  */}
             <div className="dashboard-container  sm:ml-[60px] w-full sm:w-[calc(100dvw_-_60px)] h-auto mt-[calc(72px_+_55px)] sm:mt-0 sm:h-[calc(100dvh_-_80px)] overflow-y-scroll">
-
-                <div className="flex divide-x p-5">
-                    {columns.map((column) => (
+                <div className="flex divide-x p-10">
+                    {columns.map((column, index) => (
                         <div
+                            key={index}
+                            className='w-full'
                             onDrop={(e) => handleDrop(e, column.status)}
                             onDragOver={(e) => e.preventDefault()}
                             onDragEnter={() => handleDragEnter(column.status)}
                         >
-                            <div className="flex justify-between text-3xl p-2 font-bold text-gray-500">
-                                <h2 className="capitalize">{column.status}</h2>
-                                {column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}
+                            <div className="flex justify-between text-3xl p-2 border-b">
+                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                                    {column.status}
+                                </h3>
+                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                                    {column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}
+                                </h3>
                             </div>
-                            <div className={`h-full ${currentlyHoveringOver === column.status ? 'bg-gray-200' : ''}`}>
-                                {column.tasks.map((task) => (
-                                    <TaskCard
-                                        task={task}
-                                        updateTask={updateTask}
-                                    />
-                                ))}
+                            <div className={`w-full h-full ${currentlyHoveringOver === column.status ? 'bg-gray-200' : ''}`}>
+                                {column.tasks.length !== 0 ?
+                                    column.tasks.map((task, index) => (
+                                        <div key={index}>
+                                            <TaskCard
+                                                task={task}
+                                                updateTask={updateTask}
+                                            />
+                                        </div>
+                                    ))
+                                    : (
+                                        <div className='w-full h-full flex justify-center items-center'>
+                                            <p>
+                                                No QuickDos
+                                            </p>
+                                        </div>
+                                    )
+                                }
                             </div>
                         </div>
                     ))}
                 </div>
-
             </div>
-
         </>
-
     )
 }
 
